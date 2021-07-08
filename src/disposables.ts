d8fec494cfb71e8445ee6236671d24167b53477a
guodong sun <raymon.g.sun@gmail.com>
2021-07-08 22:00:51 +0800

    feat: add tree view

--- src/disposables.ts
+++ src/disposables.ts
@@ -1,7 +1,8 @@
 import { commands, window, workspace } from "vscode";
+import { container } from "./container/inversify.config";
 import type { ViewController } from "./views/controller";
 import { VersionedFileProvider } from "./providers/versioned-file";
-import { container } from "./container/inversify.config";
+import { NodeDependenciesProvider } from "./providers/file-tree";
 
 export function createDisposables(viewController: ViewController) {
 	return [
@@ -14,6 +15,12 @@ export function createDisposables(viewController: ViewController) {
 		workspace.registerTextDocumentContentProvider(
 			VersionedFileProvider.scheme,
 			container.get(VersionedFileProvider)
+		),
+		window.registerTreeDataProvider(
+			"changedFiles",
+			new NodeDependenciesProvider(
+				workspace.workspaceFolders![0].uri.path
+			)
 		),
 	];
 }
---
+++ src/providers/file-tree.ts
@@ -0,0 +1,124 @@
+import {
+	TreeDataProvider,
+	TreeItem,
+	TreeItemCollapsibleState,
+	window,
+} from "vscode";
+import { readFileSync, accessSync } from "fs";
+import { join } from "path";
+
+export class NodeDependenciesProvider implements TreeDataProvider<Dependency> {
+	constructor(private workspaceRoot: string) {}
+
+	getTreeItem(element: Dependency): TreeItem {
+		return element;
+	}
+
+	getChildren(element?: Dependency): Thenable<Dependency[]> {
+		if (!this.workspaceRoot) {
+			window.showInformationMessage("No dependency in empty workspace");
+			return Promise.resolve([]);
+		}
+
+		if (element) {
+			return Promise.resolve(
+				this.getDepsInPackageJson(
+					join(
+						this.workspaceRoot,
+						"node_modules",
+						element.label,
+						"package.json"
+					)
+				)
+			);
+		} else {
+			const packageJsonPath = join(this.workspaceRoot, "package.json");
+			if (this.pathExists(packageJsonPath)) {
+				return Promise.resolve(
+					this.getDepsInPackageJson(packageJsonPath)
+				);
+			} else {
+				window.showInformationMessage("Workspace has no package.json");
+				return Promise.resolve([]);
+			}
+		}
+	}
+
+	/**
+	 * Given the path to package.json, read all its dependencies and devDependencies.
+	 */
+	private getDepsInPackageJson(packageJsonPath: string): Dependency[] {
+		if (this.pathExists(packageJsonPath)) {
+			const packageJson = JSON.parse(
+				readFileSync(packageJsonPath, "utf-8")
+			);
+
+			const toDep = (moduleName: string, version: string): Dependency => {
+				if (
+					this.pathExists(
+						join(this.workspaceRoot, "node_modules", moduleName)
+					)
+				) {
+					return new Dependency(
+						moduleName,
+						version,
+						TreeItemCollapsibleState.Collapsed
+					);
+				} else {
+					return new Dependency(
+						moduleName,
+						version,
+						TreeItemCollapsibleState.None
+					);
+				}
+			};
+
+			const deps = packageJson.dependencies
+				? Object.keys(packageJson.dependencies).map((dep) =>
+						toDep(dep, packageJson.dependencies[dep])
+				  )
+				: [];
+			const devDeps = packageJson.devDependencies
+				? Object.keys(packageJson.devDependencies).map((dep) =>
+						toDep(dep, packageJson.devDependencies[dep])
+				  )
+				: [];
+			return deps.concat(devDeps);
+		} else {
+			return [];
+		}
+	}
+
+	private pathExists(p: string): boolean {
+		try {
+			accessSync(p);
+		} catch (err) {
+			return false;
+		}
+		return true;
+	}
+}
+
+class Dependency extends TreeItem {
+	constructor(
+		public readonly label: string,
+		private version: string,
+		public readonly collapsibleState: TreeItemCollapsibleState
+	) {
+		super(label, collapsibleState);
+		this.tooltip = `${this.label}-${this.version}`;
+		this.description = this.version;
+	}
+
+	iconPath = {
+		light: join(
+			__filename,
+			"..",
+			"..",
+			"assets",
+			"icons",
+			"dependency.svg"
+		),
+		dark: join(__filename, "..", "..", "assets", "icons", "dependency.svg"),
+	};
+}

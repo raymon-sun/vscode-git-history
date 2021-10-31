83d653626d19967b12778449eee02c00924c8bec
raymon <raymon.g.sun@gmail.com>
2021-10-31 22:44:31 +0800

    feat: add logo and complete package.json

--- src/views/controller.ts
+++ src/views/controller.ts
@@ -45,7 +45,7 @@ export class ViewController {
 				<meta charset="UTF-8">
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src 'unsafe-inline'; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>Git Diff Picker</title>
+				<title>Git Cruise Log</title>
 			</head>
 			<body>
 				<div id="root"></div>

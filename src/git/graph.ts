3018156a6929cd694cafaf012681a62576b46178
guodong sun <82430006+raymon-sun@users.noreply.github.com>
2022-05-11 23:25:40 +0800

    refactor: #attachGraph()

--- src/git/graph.ts
+++ src/git/graph.ts
@@ -1,66 +1,38 @@
 import { Commit } from "./commit";
 
+// TODO: merge to #getCommits()
 export function attachGraph(commits: Commit[]) {
-	const branches: IBranchNode[][] = [];
-	let currentBranches: IBranchNode[][] = [];
-	/**
-	 * record commit hash to its branch chain
-	 */
-	const nodeChainMap: Record<string, IBranchNode[][]> = {};
+	/** record chains cross current commit */
+	let currentChains: IBranchNode[][] = [];
+	/** record commit hash with chains chain */
+	const nodeChainsMap: Record<string, IBranchNode[][]> = {};
 	commits.forEach((commit, index) => {
 		const { hash, parents } = commit;
-		let commitPosition: number;
-		const existedBottoms: number[] = [];
-		const lines: CommitGraphLine[] =
-			commits[index - 1]?.graph?.lines
-				.filter(({ bottom }) => {
-					if (existedBottoms.includes(bottom)) {
-						return false;
-					}
-
-					existedBottoms.push(bottom);
-
-					if (bottom !== -1) {
-						return true;
-					}
-				})
-				.map(({ bottom, color }) => ({
-					top: bottom,
-					bottom,
-					color,
-				})) || [];
+		const lines = getCurrentLines(commits[index - 1]);
 
 		const node = { hash, position: index };
-		const existedChains = nodeChainMap[hash];
+		const existedChains = nodeChainsMap[hash];
 
 		const [firstParent, ...forkParents] = parents;
+		let commitPosition: number;
 		if (existedChains) {
-			const indexList: number[] = [];
-			existedChains.forEach((existedChain) => {
-				existedChain.push(node);
-				const index = currentBranches.indexOf(existedChain);
-				if (index < 0) {
-					return;
-				}
-
-				indexList.push(index);
-			});
-
-			nodeChainMap[firstParent] = [
-				...(nodeChainMap[firstParent] || []),
-				...existedChains,
-			];
-
-			const sortedIndexList = indexList.sort();
+			// not first node of a chain
+			const sortedIndexList = getSortedIndexListForCurrentChains(
+				existedChains,
+				currentChains,
+				(chain) => chain.push(node)
+			);
 			const [firstIndex, ...otherIndexList] = sortedIndexList;
 			commitPosition = firstIndex;
+
+			pushChains(nodeChainsMap, firstParent, existedChains);
 
 			const collapseIndexList = firstParent
 				? otherIndexList
 				: sortedIndexList;
 			if (collapseIndexList.length) {
-				// remove merged branch
-				currentBranches = currentBranches.filter(
+				// remove merged chains
+				currentChains = currentChains.filter(
 					(_, i) => !collapseIndexList.includes(i)
 				);
 
@@ -75,39 +47,28 @@ export function attachGraph(commits: Commit[]) {
 				});
 			}
 		} else {
-			// first node of a branch
+			// first node of a chain
 			const newChain = [node];
-			nodeChainMap[firstParent] = [
-				...(nodeChainMap[firstParent] || []),
-				newChain,
-			];
+			pushChains(nodeChainsMap, firstParent, [newChain]);
 
-			branches.push(newChain);
-			currentBranches.push(newChain);
-			commitPosition = currentBranches.length - 1;
+			currentChains.push(newChain);
+			commitPosition = currentChains.length - 1;
 			lines.push({
 				top: -1,
-				bottom: currentBranches.length - 1,
+				bottom: currentChains.length - 1,
 				color: "red",
 			});
 		}
 
 		// handle multiple parents of the node
 		forkParents.forEach((parent) => {
-			const chains = nodeChainMap[parent];
+			const chains = nodeChainsMap[parent];
 			if (chains) {
-				// flow into the existed branch
-				const indexList: number[] = [];
-				chains.forEach((chain) => {
-					const index = currentBranches.indexOf(chain);
-					if (index < 0) {
-						return;
-					}
-
-					indexList.push(index);
-				});
-
-				const sortedIndexList = indexList.sort();
+				// flow into the existed chain
+				const sortedIndexList = getSortedIndexListForCurrentChains(
+					chains,
+					currentChains
+				);
 				const [firstIndex] = sortedIndexList;
 				if (firstIndex !== undefined) {
 					lines.push({
@@ -117,17 +78,13 @@ export function attachGraph(commits: Commit[]) {
 					});
 				}
 			} else {
-				// new branch
+				// new chain
 				const otherChain = [{ ...node }];
-				nodeChainMap[parent] = [
-					...(nodeChainMap[parent] || []),
-					otherChain,
-				];
-				currentBranches.push(otherChain);
-				branches.push(otherChain);
+				pushChains(nodeChainsMap, parent, [otherChain]);
+				currentChains.push(otherChain);
 				lines.push({
 					top: -1,
-					bottom: currentBranches.length - 1,
+					bottom: currentChains.length - 1,
 					color: "red",
 				});
 			}
@@ -135,6 +92,49 @@ export function attachGraph(commits: Commit[]) {
 
 		commit.graph = { commitPosition, lines };
 	});
+}
+
+function pushChains(
+	nodeChainsMap: Record<string, IBranchNode[][]>,
+	hash: string,
+	chain: IBranchNode[][]
+) {
+	nodeChainsMap[hash] = [...(nodeChainsMap[hash] || []), ...chain];
+}
+
+function getCurrentLines(preCommit?: Commit) {
+	const existedBottoms: number[] = [];
+	const preLines = preCommit?.graph?.lines || [];
+	return preLines
+		.filter(({ bottom }) => {
+			if (existedBottoms.includes(bottom)) {
+				return false;
+			}
+
+			existedBottoms.push(bottom);
+			return bottom !== -1;
+		})
+		.map(({ bottom, color }) => ({
+			top: bottom,
+			bottom,
+			color,
+		}));
+}
+
+function getSortedIndexListForCurrentChains(
+	chains: IBranchNode[][],
+	currentChains: IBranchNode[][],
+	iterationCallback?: (chain: IBranchNode[]) => void
+) {
+	const indexList: number[] = [];
+	chains.forEach((chain) => {
+		const index = currentChains.indexOf(chain);
+		index >= 0 && indexList.push(index);
+
+		iterationCallback?.(chain);
+	});
+
+	return indexList.sort();
 }
 
 interface IBranchNode {

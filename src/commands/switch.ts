d1c8ce0b8b865c1b762d8bf9584f93e7d57f4c5f
guodong sun <82430006+raymon-sun@users.noreply.github.com>
2022-06-11 00:18:58 +0800

    feat: complete repo pick & branch pick

--- src/commands/switch.ts
+++ src/commands/switch.ts
@@ -35,42 +35,67 @@ export function getSwitchCommandsDisposable() {
 			source.getCommits(switchSubscriber, state.logOptions);
 		}),
 		commands.registerCommand(SWITCH_REPO_COMMAND, async () => {
-			const repo = await window.showQuickPick(
-				gitService.getRepositories() || [],
-				{
-					title: "Repository Select",
-					placeHolder: "Input repo name here",
-					onDidSelectItem: (item) =>
-						window.showInformationMessage(`Focus ${item}`),
-				}
+			const quickPick = window.createQuickPick();
+
+			const items =
+				gitService
+					.getRepositories()
+					.sort()
+					.map((repo) => ({
+						label: repo,
+					})) || [];
+			quickPick.title = "Repository Select";
+			quickPick.placeholder = "Input repo name here";
+			quickPick.items = items;
+			quickPick.activeItems = items.filter(
+				({ label }) => label === state.logOptions.repo
 			);
 
-			const switchSubscriber = source.getSwitchSubscriber();
-			if (!switchSubscriber) {
-				return;
-			}
+			quickPick.onDidChangeSelection((selection) => {
+				const [item] = selection;
+				const { label: repo } = item;
+				const switchSubscriber = source.getSwitchSubscriber();
+				if (!switchSubscriber) {
+					return;
+				}
 
-			state.logOptions = { repo };
-			source.getCommits(switchSubscriber, state.logOptions);
+				state.logOptions = { repo };
+				source.getCommits(switchSubscriber, state.logOptions);
+				quickPick.dispose();
+			});
+
+			quickPick.show();
 		}),
 		commands.registerCommand(SWITCH_BRANCH_COMMAND, async () => {
-			const branch = await window.showQuickPick(
-				(await gitService.getBranches({})) || [],
-				{
-					title: "Branch Select",
-					placeHolder: "Input ref here",
-					onDidSelectItem: (item) =>
-						window.showInformationMessage(`Focus ${item}`),
-				}
+			const quickPick = window.createQuickPick();
+
+			const items =
+				((await gitService.getBranches({})) || [])
+					.sort()
+					.map((branch) => ({
+						label: branch,
+					})) || [];
+			quickPick.title = "Branch Select";
+			quickPick.placeholder = "Input reference here";
+			quickPick.items = items;
+			quickPick.activeItems = items.filter(
+				({ label }) => label === state.logOptions.ref
 			);
 
-			const switchSubscriber = source.getSwitchSubscriber();
-			if (!switchSubscriber) {
-				return;
-			}
+			quickPick.onDidChangeSelection((selection) => {
+				const [item] = selection;
+				const { label: ref } = item;
+				const switchSubscriber = source.getSwitchSubscriber();
+				if (!switchSubscriber) {
+					return;
+				}
 
-			state.logOptions = { ref: branch };
-			source.getCommits(switchSubscriber, state.logOptions);
+				state.logOptions = { ref };
+				source.getCommits(switchSubscriber, state.logOptions);
+				quickPick.dispose();
+			});
+
+			quickPick.show();
 		}),
 	];
 }
